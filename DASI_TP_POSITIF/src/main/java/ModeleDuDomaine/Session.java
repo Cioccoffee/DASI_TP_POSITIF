19d90a1dda86dc23ea325eb90798481f9e212a49
cioccoffee <gallelucie@gmail.com>
2018-03-07 15:54:11 +0100

    complétion

--- DASI_TP_POSITIF/src/main/java/ModeleDuDomaine/Session.java
+++ DASI_TP_POSITIF/src/main/java/ModeleDuDomaine/Session.java
@@ -5,10 +5,70 @@
  */
 package ModeleDuDomaine;
 
+import java.util.Date;
+
 /**
  *
  * @author vlezaud
  */
 public class Session {
+    
+    private Date debut;
+    private Date fin;
+    private String comment;
+    
+    private Client client;
+    private Employe employe;
+    private Medium medium;
+
+    public Date getDebut() {
+        return debut;
+    }
+
+    public void setDebut(Date debut) {
+        this.debut = debut;
+    }
+
+    public Date getFin() {
+        return fin;
+    }
+
+    public void setFin(Date fin) {
+        this.fin = fin;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
+    public Employe getEmploye() {
+        return employe;
+    }
+
+    public void setEmploye(Employe employe) {
+        this.employe = employe;
+    }
+
+    public Medium getMedium() {
+        return medium;
+    }
+
+    public void setMedium(Medium medium) {
+        this.medium = medium;
+    }
+    
+    
     
 }
