c440b9f4c1f966b4b0740c6da8b8e1e74cf2a55c
cioccoffee <gallelucie@gmail.com>
2018-03-11 21:45:20 +0100

    Voyant DAO pb de conversion List<Voyant> vers List<Medium> alors que héritage

--- DASI_TP_POSITIF/src/main/java/dao/VoyantDAO.java
+++ DASI_TP_POSITIF/src/main/java/dao/VoyantDAO.java
@@ -5,10 +5,38 @@
  */
 package dao;
 
-/**
- *
- * @author vlezaud
- */
+import ModeleDuDomaine.Voyant;
+import ModeleDuDomaine.Medium;
+import java.util.List;
+import javax.persistence.TypedQuery;
+
+
 public class VoyantDAO {
+    public static void creerVoyant(Voyant e)
+    {
+        JpaUtil.obtenirEntityManager().persist(e);
+    }
+     
+     public static void updateVoyant(Voyant e)
+    {
+        JpaUtil.obtenirEntityManager().merge(e);
+    }
+     
+    public static Voyant findVoyantByName(String name, String fname)
+    {
+        TypedQuery<Voyant> query = JpaUtil.obtenirEntityManager().createQuery(
+        "SELECT * FROM Voyant c WHERE c.name = :name AND c.firstname = :fname", 
+                Voyant.class);
+        query.setParameter("name", name);
+        query.setParameter("fname", fname);
+        return query.getSingleResult();
+    }
     
+    public static List<Medium> findAllVoyant()
+    {
+        TypedQuery<Voyant> query = JpaUtil.obtenirEntityManager().createQuery(
+        "SELECT * FROM Voyant", 
+                Voyant.class);
+        return query.getResultList();
+    }
 }
